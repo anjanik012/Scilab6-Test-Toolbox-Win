dde9d16ff6c45a5a67050a73a9457e7a7ad8f3a2
Anjani Kumar <anjanik012@gmail.com>
2021-03-13 18:47:06 +0530

    Correct Build script on Windows

--- test/build.ps1
+++ test/build.ps1
@@ -1,4 +1,4 @@
-gcc.exe -fPIC -c .\trans.c
-gcc.exe -shared -o libtrans.dll .\trans.o
+gcc.exe -fPIC -c -DBUILDING_EXAMPLE_DLL .\trans.c
+gcc.exe -shared -o libtrans.dll .\trans.o -Wl","--out-implib,libtrans.a
 Remove-Item .\trans.o
 gcc.exe -L.\ -Wl","-rpath=.\ .\main.c -o .\test.exe -ltrans
